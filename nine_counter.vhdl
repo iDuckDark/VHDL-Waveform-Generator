5eca8062c9d774c066ef0b8267629543e7ae1e08
pfarah65 <pfara039@uottawa.ca>
2018-11-13 21:27:12 -0500

    Fixing clock

--- nine_counter.vhdl
+++ nine_counter.vhdl
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 entity nine_counter is
 	port(
 		count: out std_logic_vector(6 downto 0);
-		EN:in std_logic;
+		--EN:in std_logic;
 		clk: in std_logic;
 		reset:in std_logic;
 		full: out std_logic
@@ -14,23 +14,20 @@ entity nine_counter is
 end entity nine_counter;
 
 architecture counter_arch of nine_counter is
-  signal counter: unsigned(6 downto 0);
+  signal r_reg: unsigned(6 downto 0);
+  signal r_next: unsigned(6 downto 0);
 begin
+	r_reg <= (others =>0);
   process(clk, reset)
     begin 
-      if(reset = '0') then 
-        counter <= "0000000";
+      if(reset = '1') then 
+        r_reg <= (others => '0');
       elsif(clk'event and clk='1') then
-        if(EN = '1') then 
-          if(counter = "1100011") then
-            counter <="0000000";
-          else 
-            counter <= counter + 1;
-          end if;
-        end if;
-    end if;         
+            r_reg <= r_next;
+      end if;       
     end process;
-    count <=std_logic_vector(counter);
-    full <= '1' when (counter ="1100011") else
+    r_next <=r_reg +1;
+    count <= std_logic_vector(r_reg);
+    full <= '1' when (r_reg ="1100011") else
       '0';  
 end counter_arch;
