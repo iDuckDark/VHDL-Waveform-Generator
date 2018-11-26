39789595a2a654438fbec15a6a293196792f94cd
pfarah65 <pfara039@uottawa.ca>
2018-11-26 14:50:08 -0500

    matlab

--- wave_generator.m
+++ wave_generator.m
@@ -1,3 +1,23 @@
+P = 1e-3;
+n = 100;
+
+Ts = P/n;
+f = 1/P;
+x = 0:Ts:P-Ts;
+
+y = sawtooth(2 * pi * f * x);
+stem(x,y)
+
+
+P = 1e-3;
+n = 100;
+
+Ts = P/n;
+f = 1/P;
+x = 0:Ts:P-Ts;
+
+y = sin(2 * pi * f * x);
+stem(x,y)
 
 
 P = 1e-3;
@@ -9,5 +29,3 @@ x = 0:Ts:P-Ts;
 
 y = square(2 * pi * f * x);
 stem(x,y)
-
-
