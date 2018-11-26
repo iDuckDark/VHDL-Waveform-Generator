a5004d1e0b7cb856416b3fdbdff291b3bf119c63
pfarah65 <pfara039@uottawa.ca>
2018-11-26 14:43:36 -0500

    Updating Matlab Code

--- wave_generator.m
+++ wave_generator.m
@@ -1,3 +1,24 @@
+P = 1e-3;
+n = 100;
+
+Ts = P/n;
+f = 1/P;
+x = 0:Ts:P-Ts;
+
+y = sawtooth(2 * pi * f * x);
+stem(x,y)
+
+
+P = 1e-3;
+n = 100;
+
+Ts = P/n;
+f = 1/P;
+x = 0:Ts:P-Ts;
+
+y = sin(2 * pi * f * x);
+stem(x,y)
+
 
 P = 1e-3;
 n = 100;
