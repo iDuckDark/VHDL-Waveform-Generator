b8aeff7318e92d1bcd0948e624b8c919378a3177
iDarkDuck <nevinganesan@hotmail.com>
2018-12-01 00:32:11 -0500

    updated screenshots

--- matlab-script-samples/wave_generator.m
+++ matlab-script-samples/wave_generator.m
@@ -1,31 +1,37 @@
+% MATLAB CODE
+
+% SIN WAVE
 P = 1e-3;
 n = 100;
-
 Ts = P/n;
 f = 1/P;
 x = 0:Ts:P-Ts;
-
-y = sawtooth(2 * pi * f * x);
-stem(x,y)
-
-
-P = 1e-3;
-n = 100;
-
-Ts = P/n;
-f = 1/P;
-x = 0:Ts:P-Ts;
-
 y = sin(2 * pi * f * x);
-stem(x,y)
+stem(x,y);
 
-
+% SQUARE WAVE
 P = 1e-3;
 n = 100;
-
 Ts = P/n;
 f = 1/P;
 x = 0:Ts:P-Ts;
-
 y = square(2 * pi * f * x);
-stem(x,y)
+stem(x,y);
+
+% TRIANGLE WAVE
+P = 1e-3;
+n = 100;
+Ts = P/n;
+f = 1/P;
+x = 0:Ts:P-Ts;
+y = sawtooth(2 * pi * f * x);
+stem(x,y);
+
+% COS WAVE
+P = 1e-3;
+n = 100;
+Ts = P/n;
+f = 1/P;
+x = 0:Ts:P-Ts;
+y = cos(2 * pi * f * x);
+stem(x,y);
